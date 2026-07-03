1aab6456b07f2a04dde777bcc497831c8361c095
Mangoo <13107921+mangoo_oo@user.noreply.gitee.com>
2026-07-03 18:17:02 +0800

    增加比大小模块

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    SERVICE WORKER - 离线缓存与更新管理
    ════════════════════════════════════════════ */
 
-const CACHE_NAME = 'kids-learning-v42';
+const CACHE_NAME = 'kids-learning-v43';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
